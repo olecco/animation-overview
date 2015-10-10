2556466676fc955fc403c9446a55185583435837
olecco <olecco@gmail.com>
2015-10-10 13:12:43 +0300

    main menu screen

--- app/src/main/java/com/olecco/android/animationoverview/MainActivity.java
+++ app/src/main/java/com/olecco/android/animationoverview/MainActivity.java
@@ -2,6 +2,7 @@ package com.olecco.android.animationoverview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +11,25 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onViewAnimationClick(View view) {
+
+    }
+
+    public void onPropertyAnimationClick(View view) {
+
+    }
+
+    public void onTransitionsFrameworkClick(View view) {
+
+    }
+
+    public void onSharedElementsClick(View view) {
+
+    }
+
+    public void onDrawableAnimationClick(View view) {
+
+    }
+
 }
