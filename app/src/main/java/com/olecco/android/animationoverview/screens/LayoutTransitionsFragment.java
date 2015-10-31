0bab7a377c751e030aaf273364e12a0889f62f10
Oleg Sedanov <sedanov@devexperts.com>
2015-10-31 16:16:17 +0300

    Custom layout transition improvement

--- app/src/main/java/com/olecco/android/animationoverview/screens/LayoutTransitionsFragment.java
+++ app/src/main/java/com/olecco/android/animationoverview/screens/LayoutTransitionsFragment.java
@@ -1,8 +1,6 @@
 package com.olecco.android.animationoverview.screens;
 
 import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.Keyframe;
 import android.animation.LayoutTransition;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
@@ -10,7 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
+import android.view.animation.OvershootInterpolator;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 
@@ -24,9 +22,9 @@ import com.olecco.android.animationoverview.views.ColoredView;
 public class LayoutTransitionsFragment extends BaseAnimationFragment {
 
     private LinearLayout viewContainer;
-    private CheckBox setTransitionCheck;
-    private CheckBox enableChangingCheck;
-    private CheckBox customAnimationsCheck;
+    private CompoundButton setTransitionCheck;
+    private CompoundButton enableChangingCheck;
+    private CompoundButton customAnimationsCheck;
 
     private final View.OnClickListener buttonClickListener = new View.OnClickListener() {
         @Override
@@ -42,6 +40,14 @@ public class LayoutTransitionsFragment extends BaseAnimationFragment {
         }
     };
 
+    private final CompoundButton.OnCheckedChangeListener checkedChangeListener =
+            new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            configureLayoutTransition();
+        }
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_transitions, container, false);
@@ -51,30 +57,14 @@ public class LayoutTransitionsFragment extends BaseAnimationFragment {
         view.findViewById(R.id.add_button).setOnClickListener(buttonClickListener);
         view.findViewById(R.id.remove_button).setOnClickListener(buttonClickListener);
 
-        setTransitionCheck = (CheckBox) view.findViewById(R.id.set_transition_check);
-        setTransitionCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                configureLayoutTransition();
-            }
-        });
+        setTransitionCheck = (CompoundButton) view.findViewById(R.id.set_transition_check);
+        setTransitionCheck.setOnCheckedChangeListener(checkedChangeListener);
 
-        enableChangingCheck = (CheckBox) view.findViewById(R.id.enable_changing_check);
-        enableChangingCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                configureLayoutTransition();
-            }
-        });
+        enableChangingCheck = (CompoundButton) view.findViewById(R.id.enable_changing_check);
+        enableChangingCheck.setOnCheckedChangeListener(checkedChangeListener);
 
-        customAnimationsCheck = (CheckBox) view.findViewById(R.id.custom_animations_check);
-        customAnimationsCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                configureLayoutTransition();
-            }
-        });
-
+        customAnimationsCheck = (CompoundButton) view.findViewById(R.id.custom_animations_check);
+        customAnimationsCheck.setOnCheckedChangeListener(checkedChangeListener);
 
         return view;
     }
@@ -114,69 +104,26 @@ public class LayoutTransitionsFragment extends BaseAnimationFragment {
     private void setupCustomAnimations() {
         LayoutTransition transition = viewContainer.getLayoutTransition();
 
-//        // Changing while Adding
-//        PropertyValuesHolder pvhLeft =
-//                PropertyValuesHolder.ofInt("left", 0, 1);
-//        PropertyValuesHolder pvhTop =
-//                PropertyValuesHolder.ofInt("top", 0, 1);
-//        PropertyValuesHolder pvhRight =
-//                PropertyValuesHolder.ofInt("right", 0, 1);
-//        PropertyValuesHolder pvhBottom =
-//                PropertyValuesHolder.ofInt("bottom", 0, 1);
-//        PropertyValuesHolder pvhScaleX =
-//                PropertyValuesHolder.ofFloat("scaleX", 1f, 0f, 1f);
-//        PropertyValuesHolder pvhScaleY =
-//                PropertyValuesHolder.ofFloat("scaleY", 1f, 0f, 1f);
-//        final ObjectAnimator changeIn = ObjectAnimator.ofPropertyValuesHolder(
-//                this, pvhLeft, pvhTop, pvhRight, pvhBottom, pvhScaleX, pvhScaleY).
-//                setDuration(transition.getDuration(LayoutTransition.CHANGE_APPEARING));
-//        transition.setAnimator(LayoutTransition.CHANGE_APPEARING, changeIn);
-//        changeIn.addListener(new AnimatorListenerAdapter() {
-//            public void onAnimationEnd(Animator anim) {
-//                View view = (View) ((ObjectAnimator) anim).getTarget();
-//                view.setScaleX(1f);
-//                view.setScaleY(1f);
-//            }
-//        });
-//        // Changing while Removing
-//        Keyframe kf0 = Keyframe.ofFloat(0f, 0f);
-//        Keyframe kf1 = Keyframe.ofFloat(.9999f, 360f);
-//        Keyframe kf2 = Keyframe.ofFloat(1f, 0f);
-//        PropertyValuesHolder pvhRotation =
-//                PropertyValuesHolder.ofKeyframe("rotation", kf0, kf1, kf2);
-//        final ObjectAnimator changeOut = ObjectAnimator.ofPropertyValuesHolder(
-//                this, pvhLeft, pvhTop, pvhRight, pvhBottom, pvhRotation).
-//                setDuration(transition.getDuration(LayoutTransition.CHANGE_DISAPPEARING));
-//        transition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, changeOut);
-//        changeOut.addListener(new AnimatorListenerAdapter() {
-//            public void onAnimationEnd(Animator anim) {
-//                View view = (View) ((ObjectAnimator) anim).getTarget();
-//                view.setRotation(0f);
-//            }
-//        });
+        // APPEARING
+        PropertyValuesHolder scaleXHolder = PropertyValuesHolder.ofFloat("scaleX", 0.0f, 1.0f);
+        PropertyValuesHolder scaleYHolder = PropertyValuesHolder.ofFloat("scaleY", 0.0f, 1.0f);
+        ObjectAnimator animAdd = ObjectAnimator.ofPropertyValuesHolder(this, scaleXHolder, scaleYHolder);
+        animAdd.setDuration(transition.getDuration(LayoutTransition.APPEARING));
+        transition.setAnimator(LayoutTransition.APPEARING, animAdd);
 
-        // Adding
-        ObjectAnimator animIn = ObjectAnimator.ofFloat(null, "rotationY", 90f, 0f);
-        animIn.setDuration(transition.getDuration(LayoutTransition.APPEARING));
-        animIn.addListener(new AnimatorListenerAdapter() {
-            public void onAnimationEnd(Animator anim) {
-                View view = (View) ((ObjectAnimator) anim).getTarget();
-                view.setRotationY(0f);
-            }
-        });
-        transition.setAnimator(LayoutTransition.APPEARING, animIn);
+        // DISAPPEARING
+        scaleXHolder = PropertyValuesHolder.ofFloat("scaleX", 1.0f, 0.0f);
+        scaleYHolder = PropertyValuesHolder.ofFloat("scaleY", 1.0f, 0.0f);
+        ObjectAnimator animRemove = ObjectAnimator.ofPropertyValuesHolder(this, scaleXHolder, scaleYHolder);
+        animAdd.setDuration(transition.getDuration(LayoutTransition.DISAPPEARING));
+        transition.setAnimator(LayoutTransition.DISAPPEARING, animRemove);
 
+        // CHANGE_APPEARING
+        Animator animator = transition.getAnimator(LayoutTransition.CHANGE_APPEARING);
+        animator.setInterpolator(new OvershootInterpolator());
 
-//        // Removing
-//        ObjectAnimator animOut = ObjectAnimator.ofFloat(null, "rotationX", 0f, 90f).
-//                setDuration(transition.getDuration(LayoutTransition.DISAPPEARING));
-//        transition.setAnimator(LayoutTransition.DISAPPEARING, animOut);
-//        animOut.addListener(new AnimatorListenerAdapter() {
-//            public void onAnimationEnd(Animator anim) {
-//                View view = (View) ((ObjectAnimator) anim).getTarget();
-//                view.setRotationX(0f);
-//            }
-//        });
+        // CHANGE_DISAPPEARING
+        transition.setStagger(LayoutTransition.CHANGE_DISAPPEARING, 400);
     }
 
     @Override
